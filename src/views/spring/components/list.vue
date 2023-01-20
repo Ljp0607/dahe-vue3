70872ff3a7e31d4330627cdcfb1623282dddd583
chen <foxchenjie14@163.com>
2023-01-20 16:34:05 +0800

    feat: 新春活动

--- src/views/spring/components/list.vue
+++ src/views/spring/components/list.vue
@@ -1,55 +1,33 @@
 <template>
   <div class="main" v-if="props.info.length > 0">
-    <div v-for="(item, index) in props.info" :key="index" class="item">
-      <div class="tab_content">
-        <div class="item_content">
-          <!--遍历数组,生成视频或图片 -->
-          <div class="item" v-for="(items, indexs) in data.item" :key="indexs">
-            <!-- 如果有视频,展示视频和播放键 -->
-            <div
-              @click="navigetDetail(items)"
-              class="video"
-              v-if="items.postsVideo"
-            >
-              <img :src="items.postsVideoImg" />
-              <img
-                class="item_start"
-                src="https://imgcdn.dahebao.cn/20221011/20221011094515515822.png"
-              />
-            </div>
-            <!-- 如果有图片,显示图片 -->
-            <div @click="navigetDetail(items)" class="img" v-else>
-              <img
-                class="postsImg"
-                :src="JSON.parse(items.postsImg)[0].imgUrl + '/pc_600'"
-              />
-            </div>
-            <div class="postsTitle">
-              <div class="hotData">
-                <span> 人气值:</span>
-                <span style="color: #dc5d3c">{{ items.hotData }}</span>
-              </div>
-              <span>{{ items.postsTitle }} </span>
-            </div>
-            <div
-              v-if="!items.ifThumb"
-              class="text"
-              @click="postThum(items, indexs)"
-            >
-              为TA投票
-            </div>
-            <div v-else class="text" @click="postThum(items, indexs)">
-              已投票
-            </div>
-          </div>
-          <img
-            v-if="data.item.length == 0"
-            class="empty"
-            src="https://imgcdn.dahebao.cn/20221027/20221027113957273185.png"
-            alt=""
-          />
+    <div class="item_content">
+      <!--遍历数组,生成视频或图片 -->
+      <div class="item" v-for="(item, index) in props.info" :key="index">
+        <!-- 如果有视频,展示视频和播放键 -->
+        <div @click="navigetDetail(item.postsId)" class="video" v-if="item.postsVideo">
+          <img :src="item.postsVideoImg" />
+          <img class="item_start" src="https://imgcdn.dahebao.cn/20221011/20221011094515515822.png" />
         </div>
+        <!-- 如果有图片,显示图片 -->
+        <div @click="navigetDetail(item.postsId)" class="img" v-else>
+          <img class="postsImg" :src="JSON.parse(item.postsImg)[0].imgUrl + '/pc_600'" />
+        </div>
+        <div class="postsTitle">
+          <div class="hotData">
+            <span> 人气值:</span>
+            <span style="color: #dc5d3c">{{ item.hotData }}</span>
+          </div>
+          <span>{{ item.postsTitle }} </span>
+        </div>
+        <div v-if="!item.ifThumb" class="text" @click="postThum(item, index)">为TA点赞</div>
+        <div v-else class="text" @click="postThum(item, index)">已投票</div>
       </div>
+      <img
+        v-if="props.info.length == 0"
+        class="empty"
+        src="https://imgcdn.dahebao.cn/20221027/20221027113957273185.png"
+        alt=""
+      />
     </div>
   </div>
   <div v-else class="empty">
@@ -57,40 +35,40 @@
   </div>
 </template>
 <script setup lang="ts">
-import { useCounterStore } from "@/stores/counter";
-import { showToast } from "vant";
-import setting from "@/common/setting";
-import { poststhumbup } from "@/api/autoShow/index";
-import { goLogin, goPosts } from "@/common/appRoute";
-const store = useCounterStore();
+import { useCounterStore } from '@/stores/counter'
+import { showToast } from 'vant'
+import setting from '@/common/setting'
+import { poststhumbup } from '@/api/autoShow/index'
+import { goLogin, goPosts } from '@/common/appRoute'
+const store = useCounterStore()
 interface Props {
   info?: Array<{
-    activityNo: string;
-    creatorType: number;
-    hotData: number;
-    postsId: number;
-    postsImg: string;
-    postsThumbUpNum: number;
-    postsTitle: string;
-    postsVideo: string;
-    postsVideoImg: string;
-    userName: string;
-    ifThumb: number;
-  }>;
+    activityNo: string
+    creatorType: number
+    hotData: number
+    postsId: number
+    postsImg: string
+    postsThumbUpNum: number
+    postsTitle: string
+    postsVideo: string
+    postsVideoImg: string
+    userName: string
+    ifThumb: number
+  }>
 }
-const props = defineProps<Props>();
+const props = defineProps<Props>()
 //投票
 function postst(index: number) {
   //如果无userId,判断环境,如果在微信,跳下载,如果浏览器环境,跳登录
-  if (store.$state.userId == "") {
+  if (store.$state.userId == '') {
     if (setting()) {
-      showToast("请在豫视频App查看详情");
+      showToast('请在豫视频App查看详情')
       setTimeout(() => {
         location.href =
-          "https://news.dahebao.cn/appdownload/index.html?Type=101&openUrl=https://news.dahebao.cn/dahe/h5/cityvote/index.html#/autoShow";
-      }, 600);
+          'https://news.dahebao.cn/appdownload/index.html?Type=101&openUrl=https://news.dahebao.cn/dahe/h5/cityvote/index.html#/autoShow'
+      }, 600)
     } else {
-      goLogin();
+      goLogin()
     }
   } else {
     //投票
@@ -102,36 +80,55 @@ function postst(index: number) {
           posts_id: props.info[index].postsId,
         })
       ).then((res: string) => {
-        if (res == "操作成功") {
-          props.info[index].hotData += 10;
-          props.info[index].ifThumb = 1;
-          showToast("投票成功");
+        if (res == '操作成功') {
+          props.info[index].hotData += 10
+          props.info[index].ifThumb = 1
+          showToast('投票成功')
         } else {
-          showToast(res);
+          showToast(res)
         }
-      });
+      })
     } else {
-      showToast("每个作品只能投一票");
+      showToast('每个作品只能投一票')
     }
+  }
+}
+//投票
+function postThum(item: any, index: number) {
+  // console.log(e);
+  //如果在微信浏览器,跳转下载页
+  if (store.$state.userId == '' && setting()) {
+    showToast('请在豫视频App投票')
+    setTimeout(() => {
+      location.href =
+        'https://news.dahebao.cn/appdownload/index.html?Type=102&openUrl=https://news.dahebao.cn/dahe/h5/cityvote/index.html#/autoShow'
+    }, 500)
+  }
+  //如果在其他浏览器,跳转下载页
+  else if (store.$state.userId == '' && !setting()) {
+    //跳转登录
+    goLogin()
+  } else {
+    postst(index)
   }
 }
 //跳转详情页
 function navigetDetail(e: number) {
   //如果在微信浏览器,跳转下载页
-  if (store.$state.userId == "" && setting()) {
-    showToast("请在豫视频App查看详情");
+  if (store.$state.userId == '' && setting()) {
+    showToast('请在豫视频App查看详情')
     setTimeout(() => {
       location.href =
-        "https://news.dahebao.cn/appdownload/index.html?Type=101&openUrl=https://news.dahebao.cn/dahe/h5/cityvote/index.html#/autoShow";
-    }, 500);
+        'https://news.dahebao.cn/appdownload/index.html?Type=101&openUrl=https://news.dahebao.cn/dahe/h5/cityvote/index.html#/autoShow'
+    }, 500)
   }
   //如果在其他浏览器,跳转下载页
-  else if (store.$state.userId == "" && !setting()) {
+  else if (store.$state.userId == '' && !setting()) {
     //跳转登录
-    goLogin();
+    goLogin()
   } else {
     //跳转帖子
-    goPosts(e);
+    goPosts(e)
   }
 }
 </script>
@@ -140,6 +137,7 @@ function navigetDetail(e: number) {
   margin: 0 auto;
   width: 665px;
   background: #fff;
+  border-radius: 0px 0px 10px 10px;
   display: flex;
   flex-wrap: wrap;
   box-sizing: border-box;
@@ -149,9 +147,11 @@ function navigetDetail(e: number) {
     margin-top: 50px;
     width: 266px;
     height: 344px;
-    background: #d7d7d7;
+    // background: #d7d7d7;
     span {
-      color: #fff;
+      // color: #fff;
+
+      color: #eb0f00;
     }
     img {
       box-sizing: border-box;
@@ -173,6 +173,118 @@ function navigetDetail(e: number) {
       background: #84b5ff;
       color: #203ab2;
       font-weight: 800;
+    }
+  }
+  .item_content {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    height: 900px;
+    .item {
+      width: 50%;
+      height: 400px;
+      margin-top: 50px;
+      position: relative;
+      // box-sizing: border-box;
+      // display: flex;
+      // flex-wrap: wrap;
+      // justify-content: space-around;
+      .video {
+        background: #ffdcdc;
+
+        border: 0;
+        margin: 0 auto;
+        width: 252px;
+        height: 276px;
+        position: relative;
+        display: flex;
+        align-items: center;
+        // border: 8px solid #4e1a02;
+        padding: 2px;
+        box-sizing: border-box;
+        img {
+          width: 100%;
+          height: auto;
+          max-height: 276px;
+        }
+        .item_start {
+          position: absolute;
+          width: 120px;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        }
+      }
+      .img {
+        background: #ffdcdc;
+        margin: 0 auto;
+        box-sizing: border-box;
+        width: 253px;
+        height: 276px;
+        display: flex;
+        align-items: center;
+        // justify-content: center;
+        .postsImg {
+          position: absolute;
+          width: 253px;
+          max-height: 276px;
+        }
+      }
+      .text {
+        background: #eb0f00;
+        margin: 20px auto 0px;
+        color: #fff;
+        line-height: 57px;
+        text-align: center;
+        width: 225px;
+        height: 57px;
+        border-radius: 50px;
+      }
+    }
+    .postsTitle {
+      box-sizing: border-box;
+      padding: 0 45px;
+      font-size: 28px;
+      color: #4e1a02;
+      font-weight: bold;
+      height: 80px;
+      span {
+        display: block;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+      }
+      .hotData {
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        // text-align: right;
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+    .hotData {
+      font-size: 26px;
+    }
+    .empty {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .button {
+    margin: 60px auto 0px;
+    width: 80%;
+    display: flex;
+    justify-content: space-around;
+    button {
+      background: none;
+      border-radius: 30px;
+      min-width: 177px;
+      height: 56px;
+      text-align: center;
+      color: #dc5d3c;
+      box-sizing: border-box;
+      border: 5px solid #dc5d3c;
     }
   }
 }
--- src/views/spring/components/tabs.vue
+++ src/views/spring/components/tabs.vue
@@ -11,11 +11,7 @@
       title-active-color="#FF0200"
       color="#FF0200"
     >
-      <van-tab
-        v-for="(item, index) in data.option"
-        :title="item.type_name"
-        :key="index"
-      >
+      <van-tab v-for="(item, index) in data.option" :title="item.type_name" :key="index">
         <List :info="data.info" />
         <div v-show="data.info.length > 0" class="button">
           <button @click="lastPage">上一页</button>
@@ -26,111 +22,121 @@
   </div>
 </template>
 <script setup lang="ts">
-import { reactive, ref, onUpdated } from "vue";
-import List from "./list.vue";
-import { selectHouse, getTypeNo } from "@/api/spring/index";
-import { showToast } from "vant";
+import { reactive, ref, onUpdated } from 'vue'
+import List from './list.vue'
+import { selectHouse, getTypeNo } from '@/api/spring/index'
+import { showToast } from 'vant'
 components: {
-  List;
+  List
 }
 interface dataType {
   option: Array<{
-    parent_type_no: string;
-    priority: number;
-    type_name: string;
-    type_no: string;
-  }>;
+    parent_type_no: string
+    priority: number
+    type_name: string
+    type_no: string
+  }>
   info?: Array<{
-    activityNo: string;
-    creatorType: number;
-    hotData: number;
-    postsId: number;
-    postsImg: string;
-    postsThumbUpNum: number;
-    postsTitle: string;
-    postsVideo: string;
-    postsVideoImg: string;
-    userName: string;
-    ifThumb: number;
-  }>;
-  resh?: string;
-  current: number;
-  more: string;
+    activityNo: string
+    creatorType: number
+    hotData: number
+    postsId: number
+    postsImg: string
+    postsThumbUpNum: number
+    postsTitle: string
+    postsVideo: string
+    postsVideoImg: string
+    userName: string
+    ifThumb: number
+  }>
+  resh?: string
+  current: number
+  more: string
 }
 const data = reactive<dataType>({
   option: [], //tabs栏
   info: [], //tab栏下的数据
   current: 1,
-  more: "more",
-});
+  more: 'more',
+})
 interface Props {
-  drawRule: string;
+  drawRule: string
 }
-const props = defineProps<Props>();
-const active = ref<number>(0);
+const props = defineProps<Props>()
+const active = ref<number>(0)
 //切换tab栏
 const changeActive = () => {
-  getCars();
-  data.current = 1;
-};
+  getCars()
+  data.current = 1
+}
 //获取当前tab栏中的车型
 const getCars = () => {
-  selectHouse(data.option[active.value].type_no, data.current).then(
-    (res: any) => {
-      if (res.state == 1) {
-        
-        data.info = res.data;
-      } else {
-        showToast(res.message);
-      }
+  selectHouse(data.option[active.value].type_no, data.current).then((res: any) => {
+    if (res.state == 1) {
+      data.info = res.data
+    } else {
+      showToast(res.message)
     }
-  );
-};
+  })
+}
 //c初始化获取tab栏
 const getParentTypeNo = () => {
   getTypeNo(props.drawRule).then(
     (
       res: Array<{
-        parent_type_no: string;
-        priority: number;
-        type_name: string;
-        type_no: string;
+        parent_type_no: string
+        priority: number
+        type_name: string
+        type_no: string
       }>
     ) => {
-      data.option = res;
-      getCars();
+      data.option = res
+      getCars()
     }
-  );
-};
+  )
+}
 //下一页新数据
 const nextPage = () => {
-  if (data.more === "more") {
-    data.current++;
-    getCars();
+  if (data.more === 'more') {
+    data.current++
+    getCars()
   } else {
-    showToast("没有更多数据了");
+    showToast('没有更多数据了')
   }
-};
+}
 //上一页数据
 const lastPage = () => {
   if (data.current <= 1) {
-    showToast("没有更多数据了");
-    return;
+    showToast('没有更多数据了')
+    return
   } else {
-    data.current--;
-    getCars();
+    data.current--
+    getCars()
   }
-};
+}
 
 onUpdated(() => {
   if (props.drawRule) {
-    getParentTypeNo();
+    getParentTypeNo()
   }
-});
+})
 </script>
 <style lang="less" scoped>
 .tabs {
   background: #d60000;
   min-height: 1021px;
+}
+
+.button {
+  margin: 15px 80px;
+  display: flex;
+  justify-content: space-around;
+  button {
+    padding: 10px 20px;
+    background-color: #fff;
+    border: 1px solid #d80000;
+    border-radius: 100px;
+    color: #d80000;
+  }
 }
 </style>
